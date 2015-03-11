57f1fb5744286df10e8d05cf0d7e36175dbc104b
Greg Roach <fisharebest@gmail.com>
2015-03-11 23:39:02 +0000

    Add Divehi (dv)

---
+++ test/Locale/LocaleDvTest.php
@@ -0,0 +1,34 @@
+<?php namespace Fisharebest\Localization;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Unit tests for the LocaleDv class
+ *
+ * @author    Greg Roach <fisharebest@gmail.com>
+ * @copyright (c) 2015 Greg Roach
+ * @license   GPLv3+
+ */
+class LocaleDvTest extends TestCase {
+	/**
+	 * Test the properties
+	 *
+	 * @return void
+	 */
+	public function testProperties() {
+		$locale = new LocaleDv;
+
+		$this->assertEquals(new LanguageDv, $locale->language());
+		$this->assertEquals(new ScriptThaa, $locale->script());
+		$this->assertEquals(new TerritoryMv, $locale->territory());
+		$this->assertNull($locale->variant());
+		$this->assertsame('unicode_ci', $locale->collation());
+		$this->assertSame('ތާނަ', $locale->endonym());
+		$this->assertSame('dv_MV', $locale->code());
+		$this->assertSame('dv', $locale->languageTag());
+		$this->assertSame('0123456789', $locale->digits('0123456789'));
+		$this->assertSame('12,345,678.09', $locale->number(12345678.09));
+		$this->assertSame('lang="dv" dir="rtl"', $locale->htmlAttributes());
+		$this->assertSame('-123', $locale->number(-123));
+	}
+}
