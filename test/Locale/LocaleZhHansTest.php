c951481668050ae52412c736c23b67b99e9db66e
Greg Roach <fisharebest@gmail.com>
2015-03-15 09:35:07 +0000

    Do not suppress the script in the language tag for zh-Hans

--- test/Locale/LocaleZhHansTest.php
+++ test/Locale/LocaleZhHansTest.php
@@ -25,7 +25,7 @@ class LocaleZhHansTest extends TestCase {
 		$this->assertsame('unicode_ci', $locale->collation());
 		$this->assertSame('0123456789', $locale->digits('0123456789'));
 		$this->assertSame('12,345,678.09', $locale->number(12345678.09));
-		$this->assertSame('lang="zh"', $locale->htmlAttributes());
+		$this->assertSame('lang="zh-Hans"', $locale->htmlAttributes());
 		$this->assertSame('-123', $locale->number(-123));
 	}
 }
